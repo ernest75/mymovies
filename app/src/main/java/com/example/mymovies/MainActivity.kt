6d1c6ed042ebdc890ece61f8d7e5d4f8392f31b4
ERNEST SCHNEIDER OLCINA <ernestschhneider@gmail.com>
2019-12-10 20:04:15 +0100

    Added list of movies in main (close #2)

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -1,12 +1,94 @@
 package com.example.mymovies
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.antonioleiva.mymovies.ui.common.CoroutineScopeActivity
+import com.example.mymovies.model.MovieDb
+import com.example.mymovies.ui.MoviesAdapter
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.BasePermissionListener
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CoroutineScopeActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val adapter = MoviesAdapter {
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        launch {
+            val location = getLocation()
+            val movies = MovieDb.service
+                .listPopularMoviesAsync(
+                    getString(R.string.api_key),
+                    getRegionFromLocation(location)
+                )
+                .await()
+            adapter.movies = movies.results
+        }
+
+        recycler.adapter = adapter
+    }
+
+    private suspend fun getLocation(): Location? {
+        val success = requestCoarseLocationPermission()
+        return if (success) findLastLocation() else null
+    }
+
+    @SuppressLint("MissingPermission")
+    private suspend fun findLastLocation(): Location? =
+        suspendCancellableCoroutine { continuation ->
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener {
+                    continuation.resume(it.result)
+                }
+        }
+
+    private suspend fun requestCoarseLocationPermission(): Boolean =
+        suspendCancellableCoroutine { continuation ->
+            Dexter
+                .withActivity(this@MainActivity)
+                .withPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(object : BasePermissionListener() {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                        continuation.resume(true)
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        continuation.resume(false)
+                    }
+                }
+                ).check()
+        }
+
+    private fun getRegionFromLocation(location: Location?): String {
+        val geocoder = Geocoder(this@MainActivity)
+        val fromLocation = location?.let {
+            geocoder.getFromLocation(
+                location.latitude,
+                location.longitude,
+                1
+            )
+        }
+        return fromLocation?.firstOrNull()?.countryCode ?: "US"
     }
 }
