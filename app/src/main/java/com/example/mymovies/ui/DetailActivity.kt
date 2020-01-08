0add05f046518730fbb323d3ccd7d08001ab0352
ERNEST SCHNEIDER OLCINA <ernestschhneider@gmail.com>
2020-01-08 19:42:17 +0100

    Refactored detail to a customview (close #10)

--- app/src/main/java/com/example/mymovies/ui/DetailActivity.kt
+++ app/src/main/java/com/example/mymovies/ui/DetailActivity.kt
@@ -20,26 +20,9 @@ class DetailActivity : AppCompatActivity() {
         with(intent.getParcelableExtra<Movie>(MOVIE)) {
             movieDetailToolbar.title = title
             movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780$backdropPath")
-
             movieDetailSummary.text = overview
+            movieDetailInfo.setMovie(this)
 
-            movieDetailInfo.text = androidx.core.text.buildSpannedString {
-
-                bold { append("Original language: ") }
-                appendln(originalLanguage)
-
-                bold { append("Original title: ") }
-                appendln(originalTitle)
-
-                bold { append("Release date: ") }
-                appendln(releaseDate)
-
-                bold { append("Popularity: ") }
-                appendln(popularity.toString())
-
-                bold { append("Vote Average: ") }
-                append(voteAverage.toString())
-            }
         }
     }
 }
