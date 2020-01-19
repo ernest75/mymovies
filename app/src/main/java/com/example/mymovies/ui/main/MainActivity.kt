57e7c8fec7cf7b58fab62a17625e0fed29179859
ERNEST SCHNEIDER OLCINA <ernestschhneider@gmail.com>
2020-01-19 19:59:02 +0100

    Refactored main to MVVM arch Components (closes #19)

--- app/src/main/java/com/example/mymovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/ui/main/MainActivity.kt
@@ -3,54 +3,49 @@ package com.example.mymovies.ui.main
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 import com.example.mymovies.R
 import com.example.mymovies.model.Movie
 import com.example.mymovies.model.MovieRepository
 import com.example.mymovies.ui.detail.DetailActivity
 import com.example.mymovies.ui.adapters.MoviesAdapter
+import com.example.mymovies.ui.main.MainViewModel.*
+import com.example.mymovies.ui.main.MainViewModel.UiModel.*
 import com.example.mymovies.ui.startActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), MainPresenter.View {
+class MainActivity : AppCompatActivity() {
 
-    private val presenter by lazy {MainPresenter(MovieRepository(this))}
-
-    private val adapter = MoviesAdapter {
-        presenter.onMovieClicked(it)
-    }
+    private lateinit var viewModel : MainViewModel
+    private lateinit var adapter : MoviesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        presenter.onCreate(this)
+        viewModel = ViewModelProviders.of(this,
+            MainViewModelFactory(MovieRepository(this))
+        )[MainViewModel::class.java]
+
+        adapter = MoviesAdapter(viewModel::onMovieClicked)
         recycler.adapter = adapter
+
+        viewModel.model.observe(this, Observer (::updateUi))
     }
 
-    override fun onDestroy() {
-        presenter.onDestroy()
-        super.onDestroy()
+    private fun updateUi(model: MainViewModel.UiModel){
+        progress.visibility = if (model == Loading) View.VISIBLE else View.GONE
 
-    }
-
-    override fun updateData(movies: List<Movie>) {
-        adapter.movies = movies
-    }
-
-    override fun navigateTo(movie: Movie) {
-        startActivity<DetailActivity> {
-            putExtra(DetailActivity.MOVIE, movie)
+        when(model){
+            is Content -> adapter.movies = model.movies
+            is Navigation -> startActivity<DetailActivity>{
+                putExtra(DetailActivity.MOVIE,model.movie)
+            }
         }
-    }
-
-    override fun showProgress() {
-       progress.visibility = View.VISIBLE
-    }
-
-    override fun hideProgress() {
-        progress.visibility = View.GONE
     }
 
 }
---
+++ app/src/main/java/com/example/mymovies/ui/main/MainViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.mymovies.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.mymovies.model.Movie
+import com.example.mymovies.model.MovieRepository
+import com.example.mymovies.ui.common.Scope
+import com.example.mymovies.ui.main.MainViewModel.UiModel.*
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val movieRepository: MovieRepository) : ViewModel(),
+    Scope by Scope.Impl() {
+
+    sealed class UiModel{
+        object Loading : UiModel()
+        class Content(val movies: List<Movie>) : UiModel()
+        class Navigation (val movie: Movie) : UiModel()
+    }
+
+    private val _model = MutableLiveData<UiModel>()
+    val model : LiveData<UiModel>
+        get() {
+            if (_model.value == null) refresh()
+            return _model
+        }
+
+    init {
+        initScope()
+    }
+
+  private fun refresh() {
+        launch {
+            _model.value = Loading
+            _model.value = Content(movieRepository.findPopularMovies().results)
+        }
+
+    }
+
+    fun onMovieClicked(movie: Movie) {
+       _model.value = Navigation(movie)
+    }
+
+    override fun onCleared() {
+        cancelScope()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    class MainViewModelFactory (private val movieRepository: MovieRepository) : ViewModelProvider.Factory{
+
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+            MainViewModel(movieRepository) as T
+
+
+    }
+
+}
