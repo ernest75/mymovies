dfb23830bb8bdf4f3859d5e66c01faa49f795c4e
ERNEST SCHNEIDER OLCINA <ernestschhneider@gmail.com>
2021-02-03 18:36:23 +0100

    Deleted steho for checking db

--- app/src/main/java/com/example/mymovies/MoviesApp.kt
+++ app/src/main/java/com/example/mymovies/MoviesApp.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import androidx.room.Room
 import com.example.mymovies.database.MovieDatabase
 import com.example.mymovies.ui.common.HyperlinkedDebugTree
-import com.facebook.stetho.Stetho
 import timber.log.Timber
 
 class MoviesApp : Application() {
@@ -19,8 +18,6 @@ class MoviesApp : Application() {
             this,
             MovieDatabase::class.java, "movie-db"
         ).build()
-
-        Stetho.initializeWithDefaults(this)
 
         if(BuildConfig.DEBUG){
             Timber.plant(HyperlinkedDebugTree())
