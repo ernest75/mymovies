62abfaa337185e5ee45730aead36ecc950816c4f
ERNEST SCHNEIDER OLCINA <ernestschhneider@gmail.com>
2021-03-05 18:25:57 +0100

    Added animation from MoviesAdapter.kt + 2nd animation from fragment + handle onBackpressed to avoid bug

--- app/src/main/java/com/example/mymovies/ui/common/Constants.kt
+++ app/src/main/java/com/example/mymovies/ui/common/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val DB_NAME = "my_movies-db"
     const val MOVIE = "DetailActivity:movie"
     const val DEFAULT_REGION = "US"
+    const val BASE_URL_PATH = "https://image.tmdb.org/t/p/w780"
 }
