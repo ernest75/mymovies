5ed18e55fbc177ddf4940feac3f7574432e3ada9
ernest75 <ernestschneider@gmail.com>
2021-06-26 13:02:13 +0200

    Fixed problems with graddle + almost finished MainIntegrationTest.kt

--- app/src/test/java/com/example/mymovies/ui/main/MainIntegrationTest.kt
+++ app/src/test/java/com/example/mymovies/ui/main/MainIntegrationTest.kt
@@ -2,20 +2,23 @@ package com.example.mymovies.ui.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.example.data.source.LocalDataSource
+import com.example.mymovies.ui.FakeLocalDataSource
 import com.example.mymovies.ui.defaultFakeMovies
 import com.example.mymovies.ui.initMockedDi
+import com.example.mymovies.ui.main.MainViewModel.*
+import com.example.testshared.mockedMovie
 import com.example.usecases.GetPopularMovies
-import com.nhaarman.mockitokotlin2.verify
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.component.get
-import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
 
 @RunWith(MockitoJUnitRunner::class)
 class MainIntegrationTest : AutoCloseKoinTest(){
@@ -24,7 +27,7 @@ class MainIntegrationTest : AutoCloseKoinTest(){
     val rule = InstantTaskExecutorRule()
 
     @Mock
-    lateinit var observer: Observer<MainViewModel.UiModel>
+    lateinit var observer: Observer<UiModel>
 
     @Mock
     lateinit var vm: MainViewModel
@@ -46,8 +49,21 @@ class MainIntegrationTest : AutoCloseKoinTest(){
 
         vm.onCoarsePermissionRequested()
 
-        verify(observer).onChanged(MainViewModel.UiModel.Content(defaultFakeMovies))
+        verify(observer).onChanged(UiModel.Content(defaultFakeMovies))
 
+    }
+
+    @Test
+    fun `data is loaded from local source when available`() {
+        val fakeMovies = listOf(mockedMovie.copy(14),mockedMovie.copy(15))
+        val localDataSource = get<LocalDataSource>() as FakeLocalDataSource
+        localDataSource.movies = fakeMovies
+
+        vm.model.observeForever(observer)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(observer).onChanged(UiModel.Content(fakeMovies))
     }
 
 }
